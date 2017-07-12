82cb0f4e4de2d930e3ca677b0bccced4bfe65677
bw <bw@Brocks-MacBook-Pro.local>
2017-07-12 00:27:25 -0500

    adding change for jsonp in order to work from different origins (requests from seperate servers)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
-import xhr from 'xhr';
 import Search from './components/Search';
 import TodaysWeather from './components/TodaysWeather';
 import FiveDayWeather from './components/FiveDayWeather';
+import fetchJsonp from 'fetch-jsonp';
 
 class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       data: {}
     };
+
   }
 
   fetchData(city) {
-
     var location = encodeURIComponent(city);
     var urlPrefix = 'http://api.openweathermap.org/data/2.5/forecast?q=';
     var urlSuffix = '&APPID=a28ad46755d6e0b7f41ced42a6a94b71&units=imperial';
@@ -23,17 +23,18 @@ class App extends React.Component {
 
     var self = this;
 
-    xhr({
-      url: url
-    }, function (err, data) {
-      self.setState({
-        data: JSON.parse(data.body)
-      });
-    });
-
+    fetchJsonp(url)
+      .then(function (response) {
+        return response.json()
+      }).then(function (json) {
+        self.setState({
+          data: json
+        });
+        console.log('parsed json', json)
+      }).catch(function (ex) {
+        console.log('parsing failed', ex)
+      })
   };
-
-
 
   render() {
     return (
